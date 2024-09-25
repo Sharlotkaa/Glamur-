b8097d6e18cabcb1f6a485c8a16e1a30add15e44
Sharlotkaa <143220115+Sharlotkaa@users.noreply.github.com>
2024-09-25 13:17:14 +0600

    Add files via upload

---
+++ csharp/1laba.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Car {
+    public string Color;
+    public int Speed;
+    public string Name;
+
+    public Car(){
+        Color = "White";
+        Speed = 200;
+        Name = "Nisan";
+    }
+
+    public Car(string color, int speed, string name){
+        Color = color;
+        Speed = speed;
+        Name = name;
+    }
+
+    public string CarInfo()
+    {
+        return $"Color - {Color}, Speed - {Speed}, Name - {Name}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car2 = new Car();
+        Car car3 = new Car("Black", 270, "BMW");
+        
+        Console.WriteLine(car2.CarInfo());
+        Console.WriteLine(car3.CarInfo());
+
+        // Ожидание ввода пользователя перед завершением программы
+        Console.ReadLine();
+    }
+}
